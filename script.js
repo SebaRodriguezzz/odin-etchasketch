270382db513a27ca638813a231c9179be97083b4
Sebastian Rodriguez <sebas.r1070@gmail.com>
2023-02-19 17:42:28 -0300

    Remove useless code

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 const grid = document.querySelector('#grid');
-const gridRows = document.createElement('div');
-const gridColumns = document.createElement('div');
-gridRows.classList.add("grid-rows");
-gridColumns.classList.add("grid-columns");
-grid.appendChild(gridRows);
-gridRows.appendChild(gridColumns);
 
 function createGrid(rows, columns){
     for (let i = 0; i < rows; i++){
         for (let j = 0; j < columns; j++){
             const square = document.createElement('div');
             square.classList.add("grid-square");
-            gridColumns.appendChild(square);
+            grid.appendChild(square);
         }
     }
 }
