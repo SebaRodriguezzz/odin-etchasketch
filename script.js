17c0940199025cff9cd2e39eac2fe3eef5bc38a0
Sebastian Rodriguez <sebas.r1070@gmail.com>
2023-02-19 19:45:37 -0300

    Add support for more grid sizes and hover effect

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const grid = document.querySelector('#grid');
 
 function createGrid(rows, columns){
+    let size = getSquareSize(rows)
     for (let i = 0; i < rows; i++){
         for (let j = 0; j < columns; j++){
             const square = document.createElement('div');
             square.classList.add("grid-square");
+            square.style.width =`${(size)}px`;
+            square.style.height =`${(size)}px`;
             grid.appendChild(square);
         }
     }
 }
 
+function getSquareSize(size){
+    return 544 / size - 2;
+}
+
 createGrid(16, 16);
+
+const squares = document.querySelectorAll('.grid-square');
+
+squares.forEach(function(square){
+    square.addEventListener('mouseover', () => {
+        square.classList.add("hover");
+})});
